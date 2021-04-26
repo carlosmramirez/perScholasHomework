053bb8a077d6207f2a509410bb49a5a7a5849afd
Carlos Ramirez <carlosmramirez17@gmail.com>
2021-04-26 15:02:13 -0400

    Initialize app and create Deck class

---
+++ mod-1-the-game/app.js
@@ -0,0 +1,35 @@
+const suits = ['spades', 'clubs', 'hearts', 'diamonds'];
+const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'K', 'Q', 'A'];
+
+class Deck {
+  constructor(suits, values) {
+    this.suits = suits;
+    this.values = values;
+    this.deck = [];
+  }
+
+  getDeck() {
+    for (let suit = 0; suit < suits.length; suit++) {
+      for (let value = 0; value < values.length; value++) {
+        let weight;
+        if (values[value] === 'J' || values[value] === 'Q' || values[value] === 'K') {
+          weight = 10;
+        } else if (values[value] === 'A') {
+          weight = 11;
+        } else {
+          weight = parseInt(values[value]);
+        }
+        const card = {
+          suit: this.suits[suit],
+          value: this.values[value],
+          weight: weight
+        }
+        this.deck.push(card);
+      }
+    }
+  }
+}
+
+const deck1 = new Deck(suits, values);
+deck1.getDeck();
+
