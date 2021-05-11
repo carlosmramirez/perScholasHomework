819c157df42f34019f7ff6c291201126cec2bef0
Carlos Ramirez <carlosmramirez17@gmail.com>
2021-05-11 09:24:08 -0400

    Homework upload

---
+++ w1h3-dr-whos-police-box/src/Components/DivTwo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import DivThree from './DivThree';
+
+class DivTwo extends React.Component {
+  render() {
+    return (
+      <div>
+        <DivThree />
+        <DivThree />
+      </div>
+    )
+  }
+}
+
+export default DivTwo;
