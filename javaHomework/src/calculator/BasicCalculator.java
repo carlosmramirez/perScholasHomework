1f740bf2871b11dbe3edfe6da39f699cd6ea64a0
Carlos Ramirez <carlosmramirez17@gmail.com>
2021-06-15 11:43:00 -0400

    Update scanner on BasicCalculator

--- javaHomework/src/calculator/BasicCalculator.java
+++ javaHomework/src/calculator/BasicCalculator.java
@@ -4,54 +4,53 @@ import java.util.Scanner;
 
 public class BasicCalculator {
 	
-	static int add() {
+	static int add(Scanner scan) {
 		int num1, num2;
-		Scanner scan = new Scanner(System.in);
+		
 		System.out.println("Please provide the 1st integer");
 		num1 = scan.nextInt();
 		
 		System.out.println("Please provide the 2nd integer");
 		num2 = scan.nextInt();
-		scan.close();
 		
 		return num1 + num2;
 	}
 	
-	static int subtract() {
+	static int subtract(Scanner scan) {
 		int num1, num2;
-		Scanner scan = new Scanner(System.in);
+//		Scanner scan = new Scanner(System.in);
 		System.out.println("Please provide the 1st integer");
 		num1 = scan.nextInt();
 		
 		System.out.println("Please provide the 2nd integer");
 		num2 = scan.nextInt();
-		scan.close();
+//		scan.close();
 		
 		return num1 - num2;
 	}
 	
-	static int multiply() {
+	static int multiply(Scanner scan) {
 		int num1, num2;
-		Scanner scan = new Scanner(System.in);
+//		Scanner scan = new Scanner(System.in);
 		System.out.println("Please provide the 1st integer");
 		num1 = scan.nextInt();
 		
 		System.out.println("Please provide the 2nd integer");
 		num2 = scan.nextInt();
-		scan.close();
+//		scan.close();
 		
 		return num1 * num2;
 	}
 	
-	static void divide() {
+	static void divide(Scanner scan) {
 		int num1, num2, quotient, remainder;
-		Scanner scan = new Scanner(System.in);
+//		Scanner scan = new Scanner(System.in);
 		System.out.println("Please provide the 1st integer");
 		num1 = scan.nextInt();
 		
 		System.out.println("Please provide the 2nd integer");
 		num2 = scan.nextInt();
-		scan.close();
+//		scan.close();
 		
 		quotient = num1 / num2;
 		remainder = num1 % num2;
@@ -62,9 +61,9 @@ public class BasicCalculator {
 		System.exit(0);
 	}
 	
-	static int getUserChoice() {
+	static int getUserChoice(Scanner scan) {
 		int userOperationChoice;
-		Scanner scan = new Scanner(System.in);
+//		Scanner scan = new Scanner(System.in);
 
 		System.out.println("Please select an integer from the list to perform an operation: \n"
 				+ "1. Add \n"
@@ -73,39 +72,44 @@ public class BasicCalculator {
 				+ "4. Divide \n"
 				+ "5. Exit");
 		userOperationChoice = scan.nextInt();
-		scan.close();
+//		scan.close();
 		
 		return userOperationChoice;
 	}
 	
 	public static void main(String[] args) {
-		int userOperationChoice = getUserChoice();
+		Scanner scan = new Scanner(System.in);
+
+		int userOperationChoice = getUserChoice(scan);
 		
 		while (userOperationChoice != 5) {
 			switch (userOperationChoice) {
 			  case 1:
-			    System.out.println("Result: " + add());
-//			    userOperationChoice = getUserChoice();
+			    System.out.println("Result: " + add(scan));
+			    userOperationChoice = getUserChoice(scan);
 			    break;
 			  case 2:
-			    System.out.println("Result: " + subtract());
-			    userOperationChoice = getUserChoice();
+			    System.out.println("Result: " + subtract(scan));
+			    userOperationChoice = getUserChoice(scan);
 			    break;
 			  case 3:
-			    System.out.println("Result: " + multiply());
-			    userOperationChoice = getUserChoice();
+			    System.out.println("Result: " + multiply(scan));
+			    userOperationChoice = getUserChoice(scan);
 			    break;
 			  case 4:
-				divide();
+				divide(scan);
 				System.out.println();
-				userOperationChoice = getUserChoice();
+				userOperationChoice = getUserChoice(scan);
 			    break;
 			  case 5: 
 				exit();
 				break;
 			  default:
 			    System.out.println("Not a valid operator integer selector!");
+			    getUserChoice(scan);
 			}
+			
 		}
+		scan.close();
 	}
 }
